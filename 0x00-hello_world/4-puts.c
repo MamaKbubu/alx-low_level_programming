e3a137b9be24d3ddffa4e1de8fe8eb7609310850
MamaKbubu <maboelatk@gmail.com>
2022-10-13 16:39:53 -0700

    Programming is like a funny puzzle

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - initialises char variables to store string then prints using puts
+ *
+ * Return : void
+ */
+int main (void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
