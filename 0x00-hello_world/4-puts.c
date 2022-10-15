a45f76512b72d643428c50891303bbceaad288b0
MamaKbubu <maboelatk@gmail.com>
2022-10-15 13:32:29 -0700

    4.puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+
 /**
-   * main - starts the char vriable to store then it prints by using puts
-   *
-   * Return : void
- */
+ * main - starts char variable to store string then prints it using puts
+ *
+ * Return: 0
+*/
 int main(void)
 {
-	puts ("\"Programming is like building a multilingual puzzle");
-	return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
