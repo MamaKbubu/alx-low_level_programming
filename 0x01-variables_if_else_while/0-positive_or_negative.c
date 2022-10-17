2a602bfe16eb5a6d6d94780ee1687ea544eb10c6
MamaKbubu <maboelatk@gmail.com>
2022-10-16 18:30:41 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,7 +16,7 @@ int main(void)
 	if (n > 0)
 		printf("%d is positive\n", n);
 	else if (n < 0)
-		print("%d is negative\n", n);
+		printf("%d is negative\n", n);
 	else
 		printf("%d id zero\n", n);
 	return (0);
