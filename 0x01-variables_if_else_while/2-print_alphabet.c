27777e601b2ff2477f278abd643a682c3c970a50
MamaKbubu <maboelatk@gmail.com>
2022-10-17 13:02:43 -0700

    Lowercase Alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.H>
+/**
+ * main - printing the alphabets in lowercase
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	char lc;
+
+	for (lc = 'a', lc < 'z', lc++)
+		putchar(lc);
+	putchar('\n');
+	return (0);
+}
