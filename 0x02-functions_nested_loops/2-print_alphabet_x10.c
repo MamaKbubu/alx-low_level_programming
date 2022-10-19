176eefb455e854b1b969abae247acdbc9aaa88e0
MamaKbubu <maboelatk@gmail.com>
2022-10-19 16:17:48 -0700

    The 1st 10 alphabets

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print the first 10 alphabets
+ *
+*/
+
+void print_alphabet_x10(void)
+{
+	char alpha
+
+		for (alpha = 'a'; alpha <= 'j' ; alpha++)
+			_putchar(alpha);
+	_putchar('\n');
+}
