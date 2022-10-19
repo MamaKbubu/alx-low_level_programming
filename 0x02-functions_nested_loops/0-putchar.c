1518cfad6861f1a4a90e2df9b1698a0cb3312c87
MamaKbubu <maboelatk@gmail.com>
2022-10-19 14:07:36 -0700

    0-putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - print _putchar
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	char line[8] = "_putchar";
+	int i;
+
+	for (l = 0 ; l <= 7; i++)
+		_puthchar(line[i]);
+	_putchar('\n');
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,6 @@
-#ifndef Main_h
-#define Main_h
-int _putchar (char c)
+#ifndef main.h
+#define main.h
+int _putchar(char c)
 
 
 #endif
