317d7ca9aa5ecda5b62a31de6471b3807980c82f
MamaKbubu <maboelatk@gmail.com>
2022-10-20 12:22:09 -0700

    0.main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - runs a test to see if integer is positive or negative
+ * Return: 0
+*/
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negtive(i);
+
+	return (0);
+}
