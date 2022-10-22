fc56f58b5ad44e412839a0e3c805fe30f24557e0
MamaKbubu <maboelatk@gmail.com>
2022-10-22 12:14:18 -0700

    alphabets

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,8 +8,10 @@
 void print_alphabet_x10(void)
 {
 	char alpha;
-	
-	for (alpha = 'a'; alpha <= 'z' ; alpha++)
+	int i;
+
+	for (i = 0; i < 10; i++)
+		for (alpha = 'a'; alpha <= 'z' ; alpha++)
 			_putchar(alpha);
 	_putchar('\n');
 }
