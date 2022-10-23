30607327b5fe21adc74bdc9a0d173055eb0b2d3b
MamaKbubu <maboelatk@gmail.com>
2022-10-23 04:11:58 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - compiles the absolute value of an integer
+ * @d: the integer to be compiled
+ * Return: The absolute value
+*/
+
+int _abs(int d)
+{
+	if (d >= 0)
+		return (d);
+	else
+		return (-d);
+}
